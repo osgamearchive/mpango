6f883f61c6874933d0ec4aae78591be7b431a2e3
mbadiac <mbadiac@adca2377-96ed-487d-85b0-23fc967ffdab>
2011-06-08 22:26:24 +0000

    modified CellBuilderTest
    git-svn-id: https://svn.code.sf.net/p/mpango/code/trunk@106 adca2377-96ed-487d-85b0-23fc967ffdab

--- mpango-core/src/test/java/net/sourceforge/mpango/directory/builder/CellBuilderTest.java
+++ mpango-core/src/test/java/net/sourceforge/mpango/directory/builder/CellBuilderTest.java
@@ -14,15 +14,15 @@ public class CellBuilderTest {
 	@Test
 	public void testBuildCell() {
 		Cell cell = TestUtils.getCell(1L);
-		CellDTO dto = new CellDTO();
-		dto.setId(cell.getIdentifier());
-		dto.setAttackBonus(cell.getAttackBonus());
-		dto.setColumn(cell.getColumn());
-		dto.setConstructions(ConstructionBuilder.instance().buildList(
-				cell.getConstructions()));
-		dto.setDefenseBonus(cell.getDefenseBonus());
-		dto.setRow(cell.getRow());
+		CellDTO dto = CellBuilder.instance().build(cell);
 		Assert.assertNotNull(dto);
+		Assert.assertEquals(dto.getId().longValue(), 1L);
+		Assert.assertEquals(dto.getConstructions().size(), 1);
+		Assert.assertEquals(dto.getAttackBonus(), new Float(cell.getAttackBonus()));
+		Assert.assertEquals(dto.getColumn(), new Integer(cell.getColumn()));
+		Assert.assertEquals(dto.getDefenseBonus(), new Float(cell.getDefenseBonus()));
+		Assert.assertEquals(dto.getRow(), new Integer(cell.getRow()));
+		
 	}
 
 }
