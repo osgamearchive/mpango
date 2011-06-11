7ba992736ff19752ca208a1a6333f5f9118d22af
sol2202 <sol2202@adca2377-96ed-487d-85b0-23fc967ffdab>
2011-06-11 09:14:46 +0000

    Make EventChannel singleton and add event processing thread
    git-svn-id: https://svn.code.sf.net/p/mpango/code/trunk@117 adca2377-96ed-487d-85b0-23fc967ffdab

--- mpango-core/src/main/java/net/sourceforge/mpango/events/channel/EventChannel.java
+++ mpango-core/src/main/java/net/sourceforge/mpango/events/channel/EventChannel.java
@@ -5,20 +5,19 @@ import net.sourceforge.mpango.events.Event;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 
-import java.util.*;
-
 /**
  * Class EventChannel encapsulates JMS topic session objects and event processing thread. 
  * @author sol2202
  *
  */
 public class EventChannel{
-	public static final EventChannel instance = new EventChannel();
+	
+	//Make the EventChannel a singleton
+	private static EventChannel instance = new EventChannel();
 	
 	private static final String JMS_TOPIC_NAME = "java:comp/env/mpangoEvents";
     private static final String MQ_URL = "tcp://localhost:61616";
 
-    private HashMap listeners = null;
     private TopicConnectionFactory connFactory = null;
     private Topic eventTopic = null;
     private TopicConnection topicConnection = null;
@@ -26,9 +25,15 @@ public class EventChannel{
     private TopicSubscriber topicSubscriber = null;
     private TopicPublisher topicPublisher = null;
 
+    private Thread eventDispatchThread = null;
+    private EventDispatcher eventDispatchTask = null;
      
     private EventChannel(){
     	
+    }
+    
+    public static EventChannel getInstance(){
+    	return instance;
     }
     
     /**
@@ -37,14 +42,16 @@ public class EventChannel{
     public void start(){
         try{
             System.out.println("Starting JMS Event Channel..");
-            connFactory = new ActiveMQConnectionFactory(MQ_URL);
+            connFactory = new ActiveMQConnectionFactory( MQ_URL );
             topicConnection = connFactory.createTopicConnection();
-            topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
-            eventTopic = topicSession.createTopic(JMS_TOPIC_NAME);
-            topicSubscriber = topicSession.createSubscriber(eventTopic);
-            topicPublisher = topicSession.createPublisher(eventTopic);
+            topicSession = topicConnection.createTopicSession( false, Session.AUTO_ACKNOWLEDGE );
+            eventTopic = topicSession.createTopic( JMS_TOPIC_NAME );
             
-            Thread eventDispatchThread = new Thread(new EventDispatcher());
+            topicSubscriber = topicSession.createSubscriber( eventTopic );
+            topicPublisher = topicSession.createPublisher( eventTopic );
+            
+            eventDispatchTask = new EventDispatcher();
+            eventDispatchThread = new Thread( eventDispatchTask );
             eventDispatchThread.start();
 
         } catch(Exception e){
@@ -58,22 +65,26 @@ public class EventChannel{
     public void stop(){
 
         System.out.println("Stopping JMS Event Channel..");
+        
+        if( eventDispatchTask != null ){
+        	eventDispatchTask.shutDown();
+        }
 
         if( topicSubscriber != null ){
             try{
                 topicSubscriber.close();
 
             } catch (JMSException e){
-                    e.printStackTrace();
-              }
+            	e.printStackTrace();
+              	}
         }
 
         if( topicPublisher != null ){
             try{
                 topicPublisher.close();
             } catch (JMSException e){
-                    e.printStackTrace();
-            }
+            	e.printStackTrace();
+            	}
         }
 
         if( topicSession != null ){
@@ -81,7 +92,7 @@ public class EventChannel{
                 topicSession.close();
             } catch (JMSException e){
                 e.printStackTrace();
-            }
+            	}
         }
 
         if( topicConnection != null ){
@@ -90,20 +101,24 @@ public class EventChannel{
             } catch (JMSException e){
                 e.printStackTrace();
                 }
-            }
-
+        }
+        
+        //Delete myself
+        instance = null;
+        
     }
-
+    
 
     /**
      * Publish specified event to the channel
+     * TODO: think about sync
      * @param event
      */
-    public synchronized void publish(final Event event){
+    public synchronized void publish( final Event event ){
         try{
             ObjectMessage eventMessage = topicSession.createObjectMessage();
-            eventMessage.setObject(event);
-            topicPublisher.publish(eventMessage);
+            eventMessage.setObject( event );
+            topicPublisher.publish( eventMessage );
 
         } catch ( Exception e ){
             e.printStackTrace();
@@ -120,48 +135,81 @@ public class EventChannel{
    static class EventDispatcher implements Runnable{
     	
 	   private boolean running = false;
+	   private ActiveMQConnectionFactory connectionFactory = null;
+	   private Connection connection = null;
+	   private Session session = null;
+	   private Destination destination = null;
+	   private MessageConsumer consumer = null;
     	
 	   public void run(){	
 		   try{
 			   //Create JMS topic connection
-			   ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(MQ_URL);   	        
-			   Connection connection = connectionFactory.createConnection();
+			   connectionFactory = new ActiveMQConnectionFactory( MQ_URL );   	        
+			   connection = connectionFactory.createConnection();
 			   connection.start();
     	        
-			   Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			   Destination destination = session.createTopic(JMS_TOPIC_NAME);
-			   MessageConsumer consumer = session.createConsumer(destination);
+			   session = connection.createSession( false, Session.AUTO_ACKNOWLEDGE );
+			   destination = session.createTopic( JMS_TOPIC_NAME );
+			   consumer = session.createConsumer( destination );
 
 			   running = true;
-
-			   while (running)
+			   
+			   // Message listening loop
+			   while ( running )
 			   {
 				  // Wait for a message
-				   Message message = consumer.receive(1000);
+				   Message message = consumer.receive( 1000 );
 
-				   if (message != null)
-				   {
-					   if (message instanceof ObjectMessage)
-					   {
-						   ObjectMessage objMessage = (ObjectMessage) message;
-						   Event event = (Event)objMessage.getObject();
-						   dispatchEvent(event);
+				   if ( message != null ){
+					   
+					   if ( message instanceof ObjectMessage ){
+						   
+						   ObjectMessage objMessage = ( ObjectMessage ) message;
+						   Object object = objMessage.getObject();
+						   
+						   if( object instanceof Event ){
+							   dispatchEvent( (Event)object ); 
+						   } 				   					   
 					   }
 				   }
     	        }
-
-    	        consumer.close();
-    	        session.close();
-    	        connection.close();
     	        
-    	      } catch (Exception e){
+    	      } catch ( Exception e ){
     	    	  e.printStackTrace();
     	      }		
     		
     	}
-    	
-	   public void dispatchEvent(Event event){
-    		
+	      
+	   public void shutDown(){
+		   
+		   if( consumer != null ){
+			   try{
+				   //This will exit the thread
+				   consumer.close(); 
+			   } catch ( JMSException e ){
+				   e.printStackTrace();
+			   }
+		   }
+		   
+		   if ( session != null ){
+			   try{
+				   session.close();
+			   } catch ( JMSException e ){
+				   e.printStackTrace();
+			   }
+		   }
+	        
+		   if ( session != null ){
+			   try{
+				   connection.close();
+			   } catch ( JMSException e ){
+				   e.printStackTrace();
+			   }
+		   }
+	   }
+	   
+	   public void dispatchEvent( Event event ){
+    		// Event processing code
 	   }
     }
 
