918d5ca8a294f82938b60b94987b48dfcdb4d70b
devdlee <devdlee@adca2377-96ed-487d-85b0-23fc967ffdab>
2011-07-03 04:49:32 +0000

    changes to handle changing password
    git-svn-id: https://svn.code.sf.net/p/mpango/code/trunk@171 adca2377-96ed-487d-85b0-23fc967ffdab

--- mpango-web/src/main/java/net/sourceforge/mpango/web/directory/ResetPasswordBackingBean.java
+++ mpango-web/src/main/java/net/sourceforge/mpango/web/directory/ResetPasswordBackingBean.java
@@ -1,19 +1,23 @@
 package net.sourceforge.mpango.web.directory;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
 import javax.jms.Session;
 
+import net.sourceforge.mpango.directory.dto.UserDTO;
 import net.sourceforge.mpango.email.SendMail;
 import net.sourceforge.mpango.jms.ActiveMQ;
 
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class ResetPasswordBackingBean {
 
 	private String email;
+	private String newPassword;
+	private String retypePassword;
+	private boolean passwordChangedFlag = false;
 	
 	public String getEmail() {
 		return email;
@@ -22,12 +26,31 @@ public class ResetPasswordBackingBean {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public String getNewPassword() {
+		return newPassword;
+	}
 
-	public static void main(String[] args) {
-		ResetPasswordBackingBean resetPasswordBackingBean = new ResetPasswordBackingBean();
-		resetPasswordBackingBean.sendMessage();
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public String getRetypePassword() {
+		return retypePassword;
+	}
+
+	public void setRetypePassword(String retypePassword) {
+		this.retypePassword = retypePassword;
 	}
 	
+	public boolean isPasswordChangedFlag() {
+		return passwordChangedFlag;
+	}
+
+	public void setPasswordChangedFlag(boolean passwordChangedFlag) {
+		this.passwordChangedFlag = passwordChangedFlag;
+	}
+
 	public void sendMessage() {
 
 		try {
@@ -37,7 +60,7 @@ public class ResetPasswordBackingBean {
 			String content = "<p>Hi there,</P>" +
 					"<p>There was recently a request to change the password on your account.</P>" +
 					"<p>If you requested this password change, please set a new password by following the link below:</P>" +
-					"<a href='http://localhost:8080/mpango-web/directory/recover.jsf'>http://localhost:8080/mpango-web/directory/recover.jsf</a>" +
+					"<a href='http://localhost:8080/mpango-web/directory/changePassword.jsf?reset_key=123'>http://localhost:8080/mpango-web/directory/recover.jsf</a>" +
 					"<p>If you don't want to change your password, just ignore this message.</P>" +
 					"<p>Thanks</p>";
 			SendMail sendMail = new SendMail(sender, recipients, subject, content);
@@ -51,5 +74,19 @@ public class ResetPasswordBackingBean {
 		} catch (JMSException e) {
 			e.printStackTrace();
 		} 
+	}
+	
+	public void changePassword() {
+		UserDTO user = new UserDTO();
+		
+		if (newPassword.equalsIgnoreCase(retypePassword)) {
+			user.setPassword(newPassword);
+			passwordChangedFlag = true;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ResetPasswordBackingBean resetPasswordBackingBean = new ResetPasswordBackingBean();
+		resetPasswordBackingBean.sendMessage();
 	}
 }
