cb3b46c0a92fdb1b39e139658cfbad69dbd10c53
devdlee <devdlee@adca2377-96ed-487d-85b0-23fc967ffdab>
2011-07-26 02:54:59 +0000

    initial import work in progress
    git-svn-id: https://svn.code.sf.net/p/mpango/code/trunk@230 adca2377-96ed-487d-85b0-23fc967ffdab

---
+++ mpango-web/src/main/java/net/sourceforge/mpango/web/directory/LoginBackingBean.java
@@ -0,0 +1,77 @@
+package net.sourceforge.mpango.web.directory;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import net.sf.mpango.common.directory.entity.User;
+import net.sf.mpango.common.directory.service.IAuthenticationService;
+
+@ManagedBean(name="loginBackingBean")
+@SessionScoped
+public class LoginBackingBean {
+	
+	private static IAuthenticationService authService;
+	private String email;
+	private String password;
+	private boolean loggedIn = false;
+
+	public String login() {
+		String action = "";
+		User user = authService.load(email);
+		if (user != null) {
+			
+			System.out.println(user.getEmail());
+			System.out.println(user.getPassword());
+			
+			if (password.equals(user.getPassword())) {
+				FacesContext context = FacesContext.getCurrentInstance();  
+				HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();  
+				HttpSession httpSession = request.getSession(false);  
+				httpSession.setAttribute("user", user);  
+				action = "success";
+				loggedIn = true;
+			} else {
+				action = "fail";
+			}
+		} else {
+			action = "fail";
+		}
+		
+		return action;
+	}
+	
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public IAuthenticationService getAuthService() {
+		return authService;
+	}
+
+	public void setAuthService(IAuthenticationService authService) {
+		this.authService = authService;
+	}
+}
