54df3723824ef681ff1f29482dea6d2e101e8e54
devdlee <devdlee@adca2377-96ed-487d-85b0-23fc967ffdab>
2011-07-26 02:57:31 +0000

    initial import work in progress
    git-svn-id: https://svn.code.sf.net/p/mpango/code/trunk@235 adca2377-96ed-487d-85b0-23fc967ffdab

---
+++ mpango-web/src/test/java/net/sourceforge/mpango/web/directory/LoginBackingBeanTest.java
@@ -0,0 +1,15 @@
+package net.sourceforge.mpango.web.directory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginBackingBeanTest {
+
+	@Test
+	public void testLogin() {
+		
+		// to be implemented
+	}
+
+}
