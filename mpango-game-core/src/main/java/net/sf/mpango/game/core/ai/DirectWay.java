cd0151e38d999ed6f7011cd52a48a6e987f1e6c4
leonserg <leonserg@adca2377-96ed-487d-85b0-23fc967ffdab>
2011-08-14 13:09:21 +0000

    completed class DirectWay and interface Path
    git-svn-id: https://svn.code.sf.net/p/mpango/code/trunk@254 adca2377-96ed-487d-85b0-23fc967ffdab

--- mpango-game-core/src/main/java/net/sf/mpango/game/core/ai/DirectWay.java
+++ mpango-game-core/src/main/java/net/sf/mpango/game/core/ai/DirectWay.java
@@ -13,20 +13,12 @@ import java.util.ArrayList;
 public class DirectWay extends ArrayList<Position> implements Path {
     private int rowSize;
     private int colSize;
-    private Position bias;
+    private Position difference;
     private Position start;
     private Position goal;
     private double direction;
 
     public DirectWay() {
-    }
-
-    public void setRowSize(int rowSize) {
-        this.rowSize = rowSize;
-    }
-
-    public void setColSize(int colSize) {
-        this.colSize = colSize;
     }
 
     public DirectWay(GameBoard board, Position start, Position goal) {
@@ -36,75 +28,74 @@ public class DirectWay extends ArrayList<Position> implements Path {
     public DirectWay(int rowSize, int colSize, Position start, Position goal) {
         this.rowSize = rowSize;
         this.colSize = colSize;
-        this.bias = new Position(0, 0);
+        this.difference = new Position(0, 0);
         this.start = start;
         this.goal = obtainCorrectCoordinates(start, goal);
         this.direction = defineDirection(start, goal);
-        this.addAll(getPath(start, goal));
+        getPath(start, goal);
     }
 
     public int isLarger(int a, int b) {
-        int inc = 0;
-        if (a > b) inc = 1;
-        if (a < b) inc = -1;
-        return inc;
+        int diff = 0;
+        if (a > b) diff = 1;
+        if (a < b) diff = -1;
+        return diff;
     }
 
-    public void defineBias(Position start, Position goal) {
-        this.bias.setRowNumber(isLarger(goal.getRowNumber(), start.getRowNumber()));
-        this.bias.setColNumber(isLarger(goal.getColNumber(), start.getColNumber()));
+    private void defineDifference(Position start, Position goal) {
+        this.difference.setRowNumber(isLarger(goal.getRowNumber(), start.getRowNumber()));
+        this.difference.setColNumber(isLarger(goal.getColNumber(), start.getColNumber()));
     }
 
-    public Position makeBias(Position start) {
-        start = makeRowBias(start);
-        start = makeColBias(start);
+    private Position makeDifference(Position start) {
+        start = makeRowDiff(start);
+        start = makeColDiff(start);
         return start;
     }
 
-    public Position makeRowBias(Position start) {
-        start.setRowNumber(start.getRowNumber() + bias.getRowNumber());
+    private Position makeRowDiff(Position start) {
+        start.setRowNumber(start.getRowNumber() + difference.getRowNumber());
         return start;
     }
 
-    public Position makeColBias(Position start) {
-        start.setColNumber(start.getColNumber() + bias.getColNumber());
+    private Position makeColDiff(Position start) {
+        start.setColNumber(start.getColNumber() + difference.getColNumber());
         return start;
     }
 
     public boolean compareDirections(Position start, Position goal) {
         double d1 = Math.abs(direction - (double) (goal.getRowNumber() - start.getRowNumber() +
-                bias.getRowNumber()) / (goal.getColNumber() - start.getColNumber()));
+                difference.getRowNumber()) / (goal.getColNumber() - start.getColNumber()));
         double d2 = Math.abs(direction - (double) (goal.getRowNumber() - start.getRowNumber()) /
-                (goal.getColNumber() - start.getColNumber() + bias.getColNumber()));
+                (goal.getColNumber() - start.getColNumber() + difference.getColNumber()));
         if (d1 > d2) return true;
         else return false;
     }
 
-    public Position getNextPosition(Position start, Position goal) {
-        if (goal.getColNumber() == start.getColNumber() || goal.getRowNumber() == start.getRowNumber()) {
-            defineBias(start, goal);
-            start = makeBias(start);
+    private Position getNextPosition(Position start, Position goal) {
+        Position next = new Position(start.getRowNumber(), start.getColNumber());
+        if (goal.getColNumber() == next.getColNumber() || goal.getRowNumber() == next.getRowNumber()) {
+            defineDifference(next, goal);
+            next = makeDifference(next);
         } else {
-            if (compareDirections(start, goal)) {
-                start = makeRowBias(start);
+            if (compareDirections(next, goal)) {
+                next = makeRowDiff(next);
             } else {
-                start = makeColBias(start);
+                next = makeColDiff(next);
             }
         }
-        return start;
+        return next;
     }
 
-    public ArrayList<Position> getPath(Position start, Position goal) {
-        ArrayList<Position> path = new ArrayList<Position>();
+    public void getPath(Position start, Position goal) {
         int length = Math.abs(goal.getRowNumber() - start.getRowNumber()) +
         Math.abs(goal.getColNumber() - start.getColNumber());
-        defineBias(start, goal);
-        path.add(new Position(start.getRowNumber(), start.getColNumber()));
+        this.add(start);
+        defineDifference(start, goal);
         for (int i = 0; i < length; i++) {
-            Position next = getNextPosition(path.get(i), goal);
-            path.add(new Position(next.getRowNumber(), next.getColNumber()));
+            Position next = getNextPosition(this.get(i), goal);
+            this.add(new Position(next.getRowNumber(), next.getColNumber()));
         }
-        return path;
     }
 
     public Position obtainCorrectCoordinates(Position start, Position goal) {
@@ -135,27 +126,45 @@ public class DirectWay extends ArrayList<Position> implements Path {
 
     @Override
     public ArrayList<Position> getParallelPath(Position start) {
-        int rowBias = start.getRowNumber() - this.start.getRowNumber();
-        int colBias = start.getColNumber() - this.start.getColNumber();
-        return getParallelPath(rowBias, colBias);
-    }
-
-    @Override
-    public ArrayList<Position> getParallelPath(int rowBias, int colBias) {
-        ArrayList<Position> parallelPath = new ArrayList<Position>();
+        start = difference(start, this.start);
+        ArrayList<Position> path = new ArrayList<Position>();
         for (Position point : this) {
-            parallelPath.add(new Position(point.getRowNumber() + rowBias, point.getColNumber() + colBias));
+            Position adding = addition(point, start);
+            path.add(new Position(adding.getRowNumber(), adding.getColNumber()));
         }
-        return parallelPath;
+        return path;
     }
 
-    public void prolong() {
+    public Position addition(Position point1, Position point2) {
+        return new Position(point1.getRowNumber() + point2.getRowNumber(), point1.getColNumber() + point2.getRowNumber());
     }
 
-    public void trim() {
+    public Position difference(Position point1, Position point2) {
+        return new Position(point1.getRowNumber() - point2.getRowNumber(), point1.getColNumber() - point2.getColNumber());
     }
 
-    @Override
+    public void prolong(int points) {
+        int times;
+        if (points > this.size()) {
+            times = points / this.size();
+        } else {
+            times = 1;
+        }
+        for (int i = 0; i < times; i++) {
+            ArrayList<Position> extra = getParallelPath(this.goal);
+            for (int j = 1; j < extra.size(); j++) {
+                if (points > 0) this.add(extra.get(j));
+                points--;
+            }
+            this.goal = this.get(this.size() - 1);
+        }
+    }
+
+    public void trim(int points) {
+        for (int i = this.size() - 1; i > 0; i--, points--) if (points > 0) this.remove(i);
+        this.goal = this.get(this.size() - 1);
+    }
+
     public double getDirection() {
         return defineDirection(this.start, this.goal);
     }
--- mpango-game-core/src/main/java/net/sf/mpango/game/core/ai/Path.java
+++ mpango-game-core/src/main/java/net/sf/mpango/game/core/ai/Path.java
@@ -2,7 +2,6 @@ package net.sf.mpango.game.core.ai;
 
 import net.sf.mpango.game.core.entity.Position;
 
-import java.sql.Array;
 import java.util.ArrayList;
 
 /**
@@ -11,10 +10,7 @@ import java.util.ArrayList;
  * Time: 12:56
  */
 public interface Path {
-    ArrayList<Position> getPath(Position start, Position goal);
     ArrayList<Position> getParallelPath(Position start);
-    ArrayList<Position> getParallelPath(int rowBias, int colBias);
-    double getDirection();
     Position getStart();
     Position getGoal();
     int size();
